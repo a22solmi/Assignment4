1cf99a2f0133130e33aa792e47572e2837119439
Sol <a22solmi@student.his.se>
2023-04-23 16:20:05 +0200

    Added text and background color to ChildActivity using intent

--- app/src/main/java/com/example/screens/ChildActivity.java
+++ app/src/main/java/com/example/screens/ChildActivity.java
@@ -1,8 +1,9 @@
 package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.constraintlayout.widget.ConstraintLayout;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ChildActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class ChildActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+
+        TextView text = findViewById(R.id.child_text);
+        ConstraintLayout layout = findViewById(R.id.layout_child);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String string = extras.getString("string");
+            int color = extras.getInt("color");
+            text.setText(string);
+            layout.setBackgroundColor(getResources().getColor(color));
+        }
     }
 }

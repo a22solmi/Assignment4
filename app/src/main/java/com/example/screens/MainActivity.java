e1b9ad41c9bd818426598b8b841e27e2e512fd20
Sol <a22solmi@student.his.se>
2023-04-23 16:17:29 +0200

    Added colors and extras for MainActivity button

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     String[] colors = {"Red", "Blue", "Green"};
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner = findViewById(R.id.spinner_view);
+        final Spinner spinner = findViewById(R.id.spinner_view);
         spinner.setOnItemSelectedListener(this);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
@@ -25,11 +26,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         adapter.setDropDownViewResource(R.layout.spinner_layout);
         spinner.setAdapter(adapter);
 
+        final HashMap<String, Integer> colorKey = new HashMap<>();
+        colorKey.put("Red", R.color.Red);
+        colorKey.put("Blue", R.color.Blue);
+        colorKey.put("Green", R.color.Green);
+
         Button button = findViewById(R.id.button_view);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String string = (String) spinner.getSelectedItem();
                 Intent intent = new Intent(MainActivity.this, ChildActivity.class);
+                intent.putExtra("string", string);
+                intent.putExtra("color", colorKey.get(string));
                 startActivity(intent);
             }
         });
